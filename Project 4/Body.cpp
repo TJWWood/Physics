0dc707331ca1fe4796ebfb0b2b77cba76dcb0de3
TJWWood <40295704@live.napier.ac.uk>
2018-10-12 11:07:45 +0100

    gitignore removed

--- Project 4/Body.cpp
+++ Project 4/Body.cpp
@@ -23,7 +23,7 @@ void Body::scale(const glm::vec3 &vect) {
 	m_mesh.scale(vect);
 }
 
-glm::vec3 applyForces(glm::vec3 x, glm::vec3 v, float t, float dt) {
+glm::vec3 Body::applyForces(glm::vec3 x, glm::vec3 v, float t, float dt) {
 	glm::vec3 fAccumulator = glm::vec3(0.0f);
 	
 	for (auto &f : m_forces)
--- Project 4/Body.h
+++ Project 4/Body.h
@@ -64,15 +64,6 @@ public:
 	void translate(const glm::vec3 &vect);
 	void rotate(float angle, const glm::vec3 &vect);
 	void scale(const glm::vec3 &vect);
+	glm::vec3 applyForces(glm::vec3 x, glm::vec3 v, float t, float dt);
 	void addForce(Force *f) { m_forces.push_back(f); }
-
-	glm::vec3 applyForces(glm::vec3 x, glm::vec3 v, float t, float dt) {
-		glm::vec3 fAccumulator = glm::vec3(0.0f);
-
-		for (auto &f : m_forces)
-		{
-			fAccumulator += f->apply(getMass(), x, v);
-		}
-		return fAccumulator / getMass();
-	}
 };
